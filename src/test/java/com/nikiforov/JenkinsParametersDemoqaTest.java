7e6ece7b01fe9e19ec7c9d635af79944ae671462
Alex Nikiforov <unleash211@gmail.com>
2024-02-14 12:27:45 +0400

    change configurations

--- src/test/java/com/nikiforov/JenkinsParametersDemoqaTest.java
+++ src/test/java/com/nikiforov/JenkinsParametersDemoqaTest.java
@@ -22,11 +22,13 @@ public class JenkinsParametersDemoqaTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("browserRemoteUrl");
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
